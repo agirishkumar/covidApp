3a34924d2882331871b8b5e82f2af479ccb7eeef
ADARI GIRISH KUMAR <adari.girishkumar@gmail.com>
2020-03-22 22:42:07 +0530

    without requirements

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-os.system('pip install -r requirements.txt')
+# os.system('pip install -r requirements.txt')
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
